6cbf06e6b185279649d1d3691ccd0bc191c4ee6f
Melissa Leite Borba <mel.leiteborba@gmail.com>
2026-07-29 16:50:13 -0300

    finalização do front-end

--- mel/helpyfast/DS-SESI-2026-1-GP2-FRONTEND/javascript/quem-sou.js
+++
@@ -1,9 +0,0 @@
-//script para salvar a escolha antes de mudar de página
-function definirTipo(tipo) {
-// salva a informação de quem que está indo para próxima página
-localStorage.setItem('tipoUsuario', tipo);
-            
-// vai para tela de login
-window.location.href = '/pages/loginpages/login.html';
-
-}
